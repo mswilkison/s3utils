517cc2a385e993f532e2c7fce52e5fa655f90015
MacLane Wilkison <mwilkison@gmail.com>
2013-02-22 21:39:16 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='s3utils',
+    version='0.0.1',
+    author='MacLane Wilkison',
+    author_email='mwilkison@gmail.com',
+    packages=['s3utils',],
+    scripts=[],
+    url='',
+    license='',
+    description='S3 Utils',
+    # long_description=open('').read(),
+    install_requires=[
+    ],
+)
